bc9cb8744accba6c921c7b88740e215e9844fd0a
VolodinAlekse <volodin.8989@mail.ru>
2023-02-03 16:20:59 +0300

    demo_02

---
+++ demo_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Random rand = new Random();
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine($"Случайное число: {number}");
+
+int LeftNumber = number / 100;
+int rightNumber = number % 10;
+
+System.Console.WriteLine($"{number/100}{number % 10}");
